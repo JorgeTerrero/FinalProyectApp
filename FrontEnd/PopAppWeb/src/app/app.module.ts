2780d84b54f701ec0d69e84662418c518f72fc17
javaChain <jorgedavidterreri@gmail.com>
2020-06-12 19:59:57 -0400

    pdfMaker Config

--- FrontEnd/PopAppWeb/src/app/app.module.ts
+++ FrontEnd/PopAppWeb/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PdfMakeWrapper } from 'pdfmake-wrapper';
+import pdfFonts from "pdfmake/build/vfs_fonts"; // fonts provided for pdfmake
+ 
+// Set the fonts to use
+PdfMakeWrapper.setFonts(pdfFonts);
 
 @NgModule({
   declarations: [
