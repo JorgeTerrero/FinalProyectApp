44aaff139c13c64e10ff9ef7e3777a1c600d3c7c
javaChain <jorgedavidterreri@gmail.com>
2020-06-18 22:13:01 -0400

    adding modules

--- FrontEnd/PopAppWeb/src/app/app.module.ts
+++ FrontEnd/PopAppWeb/src/app/app.module.ts
@@ -1,21 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule  } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PdfMakeWrapper } from 'pdfmake-wrapper';
-import pdfFonts from "pdfmake/build/vfs_fonts"; // fonts provided for pdfmake
+import pdfFonts from "pdfmake/build/vfs_fonts";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // fonts provided for pdfmake
+import {MatInputModule , MatIconModule , MatTableModule, MatButtonModule, MatPaginatorModule, MatDialogModule} from '@angular/material'
+import { from } from 'rxjs';
+import { VesselComponent } from './components/Vessel/Vessel.component';
+import { CompanyComponent  } from './components/Company/Company.component';
+import { FormsModule } from '@angular/forms';
+
  
 // Set the fonts to use
 PdfMakeWrapper.setFonts(pdfFonts);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VesselComponent,
+    CompanyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatTableModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
